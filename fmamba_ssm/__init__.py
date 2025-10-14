68d81b8e00fdddaf3adb2fc2dab87f7ec9aa88a2
Edlinf <64599457+Edlinf@users.noreply.github.com>
2025-10-14 16:10:28 +0800

    Update __init__.py

--- fmamba_ssm/__init__.py
+++ fmamba_ssm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "7.3.5c1"
+__version__ = "1.0.0"
 
 from fmamba_ssm.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
 from fmamba_ssm.modules.mamba_simple import Mamba
